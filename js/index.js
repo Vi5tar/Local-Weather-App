b41dd0357ff748642afae08512321ccd0f848620
Travis Swiers <tswiers85@gmail.com>
2017-07-29 12:49:06 -0700

    implemented fcc weather api pass through
    now should be able to connect to site using https://, obtain location
    using html5 geolocation, and get relevant weather data from fcc's api (
    https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139)

--- js/index.js
+++ js/index.js
@@ -1,120 +1,24 @@
-//openweathermap api requires the connection to be insecure due to the use of
-//a free api key. for this page to load the weather info connect to it using
-//http://
-var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?APPID=";
-var locationUrl = "http://ip-api.com/json";
-var apiKey = "c0b069518cfd9fe3d8093d9e91a529cd"
-var weatherid = "";
+var weatherUrl = "https://fcc-weather-api.glitch.me/api/current?lat=";
 var temp = "";
 
-//fetches location and weather info from their respective apis and writes
-//the appropriate info to the page
+//fetches location and weather and writes the appropriate info to the page
 $(document).ready(function() {
-  $.getJSON(locationUrl, function(location) {
-    $.getJSON(weatherUrl + apiKey + "&lat=" + location.lat +
-      "&lon=" + location.lon + "&units=metric",
-      function(weather) {
-        temp = Math.round(weather.main.temp);
-        weatherid = weather.weather[0].icon;
-        $('#cityName').html("<div class='row'>" + weather.name +
-          "</div>");
-        $('#temp').html(Math.round(weather.main.temp) +
-          "<a href='#' onclick='convertToF()'>&#8451</a>");
-        $('#description').html(weather.weather[0].description);
-        switch (weatherid) {
-          case "01d":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/01d.png'>"
-            );
-            break;
-          case "02d":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/02d.png'>"
-            );
-            break;
-          case "03d":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/03d.png'>"
-            );
-            break;
-          case "04d":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/04d.png'>"
-            );
-            break;
-          case "09d":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/09d.png'>"
-            );
-            break;
-          case "10d":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/10d.png'>"
-            );
-            break;
-          case "11d":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/11d.png'>"
-            );
-            break;
-          case "13d":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/13d.png'>"
-            );
-            break;
-          case "50d":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/50d.png'>"
-            );
-            break;
-          case "01n":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/01n.png'>"
-            );
-            break;
-          case "02n":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/02n.png'>"
-            );
-            break;
-          case "03n":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/03n.png'>"
-            );
-            break;
-          case "04n":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/04n.png'>"
-            );
-            break;
-          case "09n":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/09n.png'>"
-            );
-            break;
-          case "10n":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/10n.png'>"
-            );
-            break;
-          case "11n":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/11n.png'>"
-            );
-            break;
-          case "13n":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/13n.png'>"
-            );
-            break;
-          case "50n":
-            $("#icon").html(
-              "<img src='http://openweathermap.org/img/w/50n.png'>"
-            );
-            break;
-        }
-      });
-  });
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      $.getJSON(weatherUrl + position.coords.latitude +
+        "&lon=" + position.coords.longitude,
+        function(weather) {
+          temp = Math.round(weather.main.temp);
+          $('#cityName').html("<div class='row'>" + weather.name +
+            "</div>");
+          $('#temp').html(Math.round(weather.main.temp) +
+            "<a href='#' onclick='convertToF()'>&#8451</a>");
+          $('#description').html(weather.weather[0].description);
+          $('#icon').html("<img src='" + weather.weather[0].icon +
+            "'>");
+        });
+    });
+  };
 });
 
 //updates temp to fahrenheit
